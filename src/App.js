9d51c3ea925eb76ce9cdd675e1aa9f232ed5a816
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-01-19 04:31:57 +0300

    Create the front part of the hello app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import GreetingPage from './components/GreetingPage';
+
 function App() {
   return (
     <div className="App">
-      "Hello World"
+      <h1>Greeting page</h1>
+      <Routes>
+        <Route
+          path="/"
+          element={<GreetingPage />}
+        />
+      </Routes>
     </div>
   );
 }
